7ba46980f16d50cde30455e488a162efb36185d2
vincenzoarena <penninoematita@gmail.com>
2022-08-11 13:29:45 +0200

    2022-08-11 - Repository created and structure implemented

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Transaction = ({ transaction }) => {
+  return (
+    <li className="minus">
+      {transaction.text} <span>-€400</span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};
